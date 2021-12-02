99d96b603732bd0c9b38a85e503dc80252bf5ab8
Yann Spadari <91940990+yann-spadari@users.noreply.github.com>
2021-12-02 10:49:35 +0100

    Update config.php

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ try
     
     // Connexion à la base de donnée
     
-    $db = new PDO('mysql:host=localhost;dbname=moduleconnexion;', 'root', '');
+    $db = new PDO('mysql:host=localhost;dbname=yann-spadari_moduleconnexion;', 'gecks', 'Gecks13013@');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 }
